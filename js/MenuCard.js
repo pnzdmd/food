723dfea705d02ce806ec1d390d74372dd827158e
Oleg Novikov <pnzdmd@yandex.ru>
2022-03-12 18:59:17 +0300

    Рефакторинг class MenuCard

--- js/MenuCard.js
+++ js/MenuCard.js
@@ -1,10 +1,11 @@
 export class MenuCard {
-  constructor(src, alt, title, descr, price, parentSelector) {
+  constructor(src, alt, title, descr, price, parentSelector, ...classes) {
     this.src = src;
     this.alt = alt;
     this.title = title;
     this.descr = descr;
     this.price = price;
+    this.classes = classes;
     this.parent = document.querySelector(parentSelector);
 
     this.transfer = 100;
@@ -17,8 +18,15 @@ export class MenuCard {
 
   render() {
     const element = document.createElement('div');
+    
+    if(this.classes.length === 0) {
+      this.element = 'menu__item';
+      element.classList.add(this.element);
+    } else {
+      this.classes.forEach(className => element.classList.add(className));
+    }
+
     element.innerHTML = `
-    <div class="menu__item">
       <img src=${this.src} alt=${this.alt}>
       <h3 class="menu__item-subtitle">${this.title}</h3>
       <div class="menu__item-descr">${this.descr}</div>
@@ -27,7 +35,6 @@ export class MenuCard {
         <div class="menu__item-cost">Цена:</div>
         <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
       </div>
-    </div>
     `;
     this.parent.append(element);
   }
