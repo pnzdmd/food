e0e09d43d8f1c66fabe33c494a516cfba1dfc6f8
Oleg Novikov <pnzdmd@yandex.ru>
2022-03-12 13:02:16 +0300

    Открытие модального окна через опреденное время

--- js/main.js
+++ js/main.js
@@ -115,29 +115,53 @@ window.addEventListener('DOMContentLoaded', () => {
   const modalCloseBtn = document.querySelector('[data-close]');
 
 
+  //открытие модального окна
+  function openModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = 'hidden';
+    // модальное окно не откроется через опреденное время после открытия
+    clearInterval(modalTimerId);
+  }
   modalTrigger.forEach(btn => {
-    btn.addEventListener('click', () => {
-      modal.classList.toggle('show');
-      document.body.style.overflow = 'hidden';
-    });
+    btn.addEventListener('click', openModal);
   });
-  
+
+
+  // закрытие модального окна
   function closeModal() {
     modal.classList.toggle('show');
     document.body.style.overflow = '';
   }
   modalCloseBtn.addEventListener('click', closeModal);
 
+  // закрытие модального окна по клику на оверлей
   modal.addEventListener('click', (evt) => {
     if (evt.target === modal) {
       closeModal();
     }
   });
 
+  // закрытие модального окна нажатием Esc
   document.addEventListener('keydown', (evt) => {
     if(evt.code === "Escape" && modal.classList.contains('show')) {
       closeModal();
     }
   });
   
+  // открытие модального окна через опреденное время
+  const modalTimerId = setTimeout( openModal, 3000);
+
+  // открытие модального окна в конце страницы
+  function showModalbyScroll() {
+    if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      // что бы модальное окно не открывалось каждый раз в конце страницы
+      window.removeEventListener('scroll', showModalbyScroll);
+    }
+  }
+
+  
+  window.addEventListener('scroll', showModalbyScroll);
+
+
 });
